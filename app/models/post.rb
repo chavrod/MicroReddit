5c0a60220e947929d08b14f1c25637bd848f4e7c
Dima Chavro <dchavro@gmail.com>
2022-06-13 21:39:38 +0100

    create association between Users and Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
   validates :title, :body, presence: true
   validates :title, :body, uniqueness: true
   validates :body, length: { minimum: 5 }
+
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   validates :name, :email, :password, uniqueness: true
   validates :name, length: { minimum: 3, maximum: 12 }
   validates :password, length: { minimum: 6, maximum: 16 }
+
+  has_many :posts, dependent: :destroy
 end
